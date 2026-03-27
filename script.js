0ab5489124a96192f973b4a0aab3e0f307aaffed
smile4909 <smile4909@users.noreply.github.com>
2026-03-27 12:22:05 +0700

    Update ceremony times in index.html and sync script.js countdown

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ function initPage() {
    3. COUNTDOWN TIMER
 ======================== */
 function startCountdown() {
-  const weddingDate = new Date('2026-03-29T10:00:00');
+  const weddingDate = new Date('2026-03-29T09:00:00');
 
   function update() {
     const now = new Date();
